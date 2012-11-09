c4abfa39cd7cbfe838c23da7188ea7ac42ab9187
anon4ik <anon@anonymous.lv>
2012-11-09 13:27:30 +0200

    libriffoser.c stub

---
+++ libriffoser.c
@@ -0,0 +1,18 @@
+/*
+**** Riffoser source has been refactored and split to separate files
+
+** CORE 
+ * track.c - functions for tracks
+ * wave.c - functions for wave
+ * instrument.c - functions for instruments
+
+** IO
+ * wav.c - functions for saving/loading WAV files
+ * ogg.c - functions for saving/loading OGG files
+ * mp3.c - functions for saving/loading MP3 files
+
+ ** HEADERS
+ * riffoser.h - system header to be installed to include dir
+ * riffoser_r.h - header containing run-time stuff for library compilation only
+
+ */
