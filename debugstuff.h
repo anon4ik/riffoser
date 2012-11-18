090a96b0731703b3cda23bb75ee6e19abeb60385
anon4ik <anon@anonymous.lv>
2012-11-18 00:42:25 +0000

    add debugstuff.h

---
+++ debugstuff.h
@@ -0,0 +1,15 @@
+#ifndef DEBUGSTUFF_H
+#define	DEBUGSTUFF_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+// nothing to see here, move along
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* DEBUGSTUFF_H */
+
--- riffoser.h
+++ riffoser.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <debugstuff.h>
 
 #ifdef USE_OGGVORBIS
 #include <ogg/ogg.h>
